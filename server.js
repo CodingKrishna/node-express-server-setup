d15a972de0700dc08c4e9386739ba432a6a3240b
codingkrishna <codingkrishna@gmail.com>
2023-12-08 07:15:30 +0530

    misc changes

--- server.js
+++ server.js
@@ -19,6 +19,28 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-http.createServer(app).listen(app.get('port'), function(){
+
+let users = [{ id: 1, name: "ravi"}, {id: 2, name: "siva" }]
+app.get('/index.html', function (req, res) {
+  res.sendFile(__dirname + "/" + "index.html");
+})
+
+app.post('/createUser', function (req, res) {
+  // Prepare output in JSON format
+  let user = {
+    id: req.body.id,
+    name: req.body.name
+  }
+  users.push(user);
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(user));
+})
+
+app.get('/listUsers', function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(users));
+})
+
+http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
