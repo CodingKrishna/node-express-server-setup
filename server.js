8704dc454a0caffc4fafeac6564c2c71de1fa501
codingkrishna <codingkrishna@gmail.com>
2023-12-08 07:42:26 +0530

    create user

--- server.js
+++ server.js
@@ -8,11 +8,18 @@ var http = require('http');
 var path = require('path');
 var fs = require("fs");
 var app = express();
+var bodyParser = require('body-parser');
 
 // all environments
 app.set('port', process.env.PORT || 4300);
 app.use(express.logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // development only
 if ('development' == app.get('env')) {
@@ -20,10 +27,8 @@ if ('development' == app.get('env')) {
 }
 
 
-let users = [{ id: 1, name: "ravi"}, {id: 2, name: "siva" }]
-app.get('/index.html', function (req, res) {
-  res.sendFile(__dirname + "/" + "index.html");
-})
+let users = [{ id: 1, name: "ravi"}, {id: 2, name: "siva" }];
+
 
 app.post('/createUser', function (req, res) {
   // Prepare output in JSON format
